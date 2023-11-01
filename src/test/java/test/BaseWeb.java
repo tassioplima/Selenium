ac339167169c529fadd0bf89066f5a917707e9ce
Tassio Lima <tassiolima@Tassios-MBP.lan>
2023-11-01 12:30:59 +0000

    update

--- src/test/java/test/BaseWeb.java
+++ src/test/java/test/BaseWeb.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
-import org.t
 public class BaseWeb {
     protected WebDriver driver;
 
