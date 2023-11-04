c0e68529a5c6a5095145a3db9b9140361052ad25
Tassio Lima <tassiolima@Tassios-MBP.lan>
2023-11-04 00:58:33 +0000

    update

--- src/test/java/driver/OptionsManager.java
+++ src/test/java/driver/OptionsManager.java
@@ -13,19 +13,7 @@ public class OptionsManager {
         options.addArguments("--disable-infobars");
         options.addArguments("--remote-allow-origins");
         options.addArguments("--disable-popup-blocking");
-
-        return options;
-    }
-    public static ChromeOptions chromeOptionsHeadless(){
-
-        var options = new ChromeOptions();
-        options.addArguments("--disable-notifications");
-        options.addArguments("--start-fullscreen");
-        options.addArguments("--disable-infobars");
-        options.addArguments("--remote-allow-origins");
-        options.addArguments("--disable-popup-blocking");
-        options.addArguments("--headless=new");
-
+        if (BrowserManager.HEADLESS.getBool()) options.addArguments("--headless=new");
         return options;
     }
 
@@ -36,19 +24,7 @@ public class OptionsManager {
         options.addArguments("--start-fullscreen");
         options.addArguments("--disable-infobars");
         options.addArguments("--disable-popup-blocking");
-
-        return options;
-    }
-
-    public static FirefoxOptions firefoxOptionsHeadless(){
-
-        var options = new FirefoxOptions();
-        options.addArguments("--disable-notifications");
-        options.addArguments("--start-fullscreen");
-        options.addArguments("--disable-infobars");
-        options.addArguments("--disable-popup-blocking");
-        options.addArguments("--headless");
-
+        if (BrowserManager.HEADLESS.getBool()) options.addArguments("--headless");
         return options;
     }
 
