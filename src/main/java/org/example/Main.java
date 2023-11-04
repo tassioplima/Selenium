2c7d5b20c4506d31a464d8b50d794eb9d03910b0
Tassio Lima <tassiolima@Tassios-MBP.lan>
2023-11-04 00:30:17 +0000

    update main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,5 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String classpath = System.getProperty("java.class.path");
-        System.out.println(classpath);
     }
+}
